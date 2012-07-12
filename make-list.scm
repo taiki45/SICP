ba38707e4747f859b539aa1b43de83a5164c840d
Taiki ONO <taiks.4559@gmail.com>
2012-07-12 23:43:52 +0900

    add argument as proc

--- make-list.scm
+++ make-list.scm
@@ -1,4 +1,8 @@
-(define (make-list init limit)
-  (if (= init limit)
-    (list limit)
-    (append (list init) (make-list (+ init 1) limit))))
+(provide #f)
+(define (make-list proc init limit)
+  (cond ((= init limit) (cons (proc limit)
+                              '()))
+        (else (cons (proc init)
+                    (make-list proc
+                               (+ 1 init)
+                               limit)))))
