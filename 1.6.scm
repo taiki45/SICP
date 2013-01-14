d9f738e1b36ea2ab91a688d86adff9d2ed429c24
Taiki ONO <taiks.4559@gmail.com>
2013-01-15 02:56:38 +0900

    add sample code

--- 1.6.scm
+++ 1.6.scm
@@ -2,3 +2,9 @@
 ; `if` does not evaluates then and else clauses
 ;
 ; If define `new-if` I use `lambda` and `macro` for clauses.
+
+(define-macro (new-if predicate then-clause else-clause)
+  `(cond (,predicate (lambda () ,then-clause))
+    (else (lambda () ,else-clause))))
+
+; This works?
