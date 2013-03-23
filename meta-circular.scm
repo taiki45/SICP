d5e4a523e3abe4363e6f68a502a9652c6b1e4c1c
Taiki ONO <taiks.4559@gmail.com>
2013-03-24 03:17:16 +0900

    add apply

--- meta-circular.scm
+++ meta-circular.scm
@@ -17,3 +17,16 @@
                  (list-of-values (operands exp) env)))
         (else
           (error "Unknown expression type --EVAL" exp))))
+
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+           (procedure-body procedure)
+           (extend-environment
+             (procedure-parameters procedure)
+             arguments
+             (procedure-environment procedure))))
+        (else
+          (error "Unknown procedure type -- APPLY" procedure))))
