98417202e01b9d52425cf2611ab1ee5900e3af16
Taiki ONO <taiks.4559@gmail.com>
2013-03-24 03:23:27 +0900

    add list-of-values

--- meta-circular.scm
+++ meta-circular.scm
@@ -30,3 +30,9 @@
              (procedure-environment procedure))))
         (else
           (error "Unknown procedure type -- APPLY" procedure))))
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+    '()
+    (cons (eval (first-operand exps) env)
+          (list-of-values (rest-operands exps) env))))
